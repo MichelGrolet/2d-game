064e975f87aac5e8ce11554e229f8ea1b29008b9
Louis <82098276+MonsieurMiam@users.noreply.github.com>
2021-06-07 14:37:23 +0200

    Update Labyrinthe.java

--- jeuZeldiablo/application/Labyrinthe.java
+++ jeuZeldiablo/application/Labyrinthe.java
@@ -2,11 +2,13 @@ package application;
 
 public class Labyrinthe {
 	private static int taille = 10;
-	private Liste<Case> cases;
+	private Case[][] cases;
 	
 	public Labyrinthe() {
 		for(int i=0; i<taille; i++) {
-			cases.add(new Sol());
+			for(int j=0; j<taille; j++) {
+				cases[i][j] = new Sol();
+			}
 		}
 	}
 }
