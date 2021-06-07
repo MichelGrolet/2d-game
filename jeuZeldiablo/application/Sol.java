8f86be51111f3b06ca5c41c4e6942f81e2b79cbf
Gabin Chassard <gabin.chassard2@etu.univ-lorraine.fr>
2021-06-07 14:38:49 +0200

    Attributs et constructeur de sol

--- jeuZeldiablo/application/Sol.java
+++ jeuZeldiablo/application/Sol.java
@@ -2,3 +2,38 @@
 * Classe representant du sol
 * implemente Case
 */
+public class Sol implements Case{
+
+    /**
+    * Coordonnee x de la case
+    */
+    private int x;
+
+    /**
+    * Coordonnee y de la case
+    */
+    private int y;
+
+    /**
+    * Constructeur d'une case de sol
+    * @param x coordonnee x de la case
+    * @param y coordonnee y de la case
+    */
+    public Sol(int x,int y){
+        //Instancie a zero si le parametre est negatif
+        if(x<0){
+            this.x=0;
+        }
+        else{
+            this.x=x;
+        }
+
+        //Instancie a zero si le parametre est negati
+        if(y<0){
+            this.y=0;
+        }
+        else{
+            this.y=y;
+        }
+    }
+}
