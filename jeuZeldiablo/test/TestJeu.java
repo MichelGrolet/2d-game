443121a492338da09152b7f2ff7f7994d8df5ea6
Theo Hornberger <theo.hornberger@gmail.com>
2021-06-07 17:46:40 +0200

    m

--- jeuZeldiablo/test/TestJeu.java
+++ jeuZeldiablo/test/TestJeu.java
@@ -1,5 +1,20 @@
 package test;
 
-public class TestJeu {
+import application.*;
+import static org.junit.Assert.*;
+import org.junit.Test;
 
+public class TestJeu {
+	
+	@Test
+	public void testConstructerVide() {
+		Labyrinthe la = new Labyrinthe();
+		Case[][] c = new Case[10][10];
+		for(int i  =0; i<10; i++) {
+			for(int j=0; j<10; j++) {
+				c[i][j] = new Sol(i, j);
+			}
+		}
+ 		assertEquals("devrait un tableau de case", c, la.getTabCase());
+	}
 }
