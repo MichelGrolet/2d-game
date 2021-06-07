b450377af3576568659a53bf12dc19da88a26995
Gabin Chassard <gabin.chassard2@etu.univ-lorraine.fr>
2021-06-07 15:30:41 +0200

    Creation des classes Case et Sol

---
+++ jeuZeldiablo/application/Sol.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+/**
+* Classe representant du sol
+* herite de Case
+*/
+public class Sol extends Case{
+    /**
+    * Constructeur d'une case de sol
+    * @param x coordonnee x de la case
+    * @param y coordonnee y de la case
+    */
+    public Sol(int x,int y){
+        super(x,y);
+
+        //Couleur du sol : gris
+        this.color=Color.GRAY;
+    }
+}
