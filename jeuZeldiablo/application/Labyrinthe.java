dbcc0d6be2bcf71c63836d87c029e087ef1f22b1
Louis <82098276+MonsieurMiam@users.noreply.github.com>
2021-06-07 14:39:44 +0200

    Revert "Update Labyrinthe.java"
    This reverts commit 064e975f87aac5e8ce11554e229f8ea1b29008b9.

--- jeuZeldiablo/application/Labyrinthe.java
+++ jeuZeldiablo/application/Labyrinthe.java
@@ -2,13 +2,11 @@ package application;
 
 public class Labyrinthe {
 	private static int taille = 10;
-	private Case[][] cases;
+	private Liste<Case> cases;
 	
 	public Labyrinthe() {
 		for(int i=0; i<taille; i++) {
-			for(int j=0; j<taille; j++) {
-				cases[i][j] = new Sol();
-			}
+			cases.add(new Sol());
 		}
 	}
 }
