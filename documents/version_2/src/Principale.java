d45db471e82f9bb8b0d49d4922b0d3bcff714d32
grolet4u <grolet4u@etu.univ-lorraine.fr>
2021-06-07 11:18:07 +0200

    v2

---
+++ documents/version_2/src/Principale.java
@@ -0,0 +1,7 @@
+package src;
+
+public class Principale {
+    public static void main(String[] args) {
+        Jeu jeu = new Jeu();
+    }
+}
---
+++ documents/version_2/test/TestPersonnage.java
@@ -0,0 +1,18 @@
+package test;
+
+import src.*;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ * Classe de test du Personnage
+ *
+ */
+public class TestPersonnage{
+    @Test
+    public void testConstructeurNegatif() {
+        Personnage p = new Personnage();
+        assertEquals("edzsfre", true, true)
+    }
+}
