d1bd0e9e898cdaacc6fe8fe14697f3cd2229d75f
Gabin Chassard <gabin.chassard2@etu.univ-lorraine.fr>
2021-06-08 11:38:16 +0200

    ajout d'un test pour Labyrinthe

--- jeuZeldiablo/test/TestLabyrinthe.java
+++ jeuZeldiablo/test/TestLabyrinthe.java
@@ -8,6 +8,9 @@ import java.util.ArrayList;
 import org.junit.Test;
 
 public class TestLabyrinthe {
+	/**
+	* Test du constructeur
+	*/
 	@Test
 	public void testConstructerVide() {
 		Labyrinthe la = new Labyrinthe();
@@ -17,7 +20,8 @@ public class TestLabyrinthe {
 	/**
 	* Test les differents pattern de labyrinthe
 	*/
-	public void testGenererMur(){
+	@Test
+	public void testGenererContour(){
 		Labyrinthe l1=new Labyrinthe();
 		Labyrinthe l2=new Labyrinthe();
 		Labyrinthe l3=new Labyrinthe();
@@ -64,13 +68,13 @@ public class TestLabyrinthe {
 
 		if(res1 || res2 || res3){
 			for(int j=0;j<Labyrinthe.TAILLE;j++){
-				if(l1.getCase(20,j) instanceof Sol){
+				if(l1.getCase(Labyrinthe.TAILLE-1,j) instanceof Sol){
 					res1=false;
 				}
-				if(l2.getCase(20,j) instanceof Sol){
+				if(l2.getCase(Labyrinthe.TAILLE-1,j) instanceof Sol){
 					res2=false;
 				}
-				if(l3.getCase(20,j) instanceof Sol){
+				if(l3.getCase(Labyrinthe.TAILLE-1,j) instanceof Sol){
 					res3=false;
 				}
 				if(!res1 && !res2 && !res3){
@@ -81,13 +85,13 @@ public class TestLabyrinthe {
 
 		if(res1 || res2 || res3){
 			for(int i=0;i<Labyrinthe.TAILLE;i++){
-				if(l1.getCase(i,20) instanceof Sol){
+				if(l1.getCase(i,Labyrinthe.TAILLE-1) instanceof Sol){
 					res1=false;
 				}
-				if(l2.getCase(i,20) instanceof Sol){
+				if(l2.getCase(i,Labyrinthe.TAILLE-1) instanceof Sol){
 					res2=false;
 				}
-				if(l3.getCase(i,20) instanceof Sol){
+				if(l3.getCase(i,Labyrinthe.TAILLE-1) instanceof Sol){
 					res3=false;
 				}
 				if(!res1 && !res2 && !res3){
@@ -99,5 +103,24 @@ public class TestLabyrinthe {
 		assertTrue("Le pattern 1 n'est pas bon",res1);
 		assertTrue("Le pattern 2 n'est pas bon",res2);
 		assertTrue("Le pattern 3 n'est pas bon",res3);
+	}
+
+	/**
+	* Test d'un mauvais parametre
+	*/
+	@Test
+	public void testMauvaisParam(){
+		Labyrinthe l=new Labyrinthe();
+		boolean res=true;
+
+		l.genererMur(5);
+
+		for(int i=0;i<Labyrinthe.TAILLE;i++){
+			if(l.getCase(i,0) instanceof Sol){
+				res=false;
+			}
+		}
+
+		assertTrue("Il devrait y avoir des murs",res);
 	}
 }
