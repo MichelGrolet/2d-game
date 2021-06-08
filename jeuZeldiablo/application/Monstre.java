bc4e0a816887e414e7afd8e55a8a74090110d810
grolet4u <grolet4u@etu.univ-lorraine.fr>
2021-06-08 10:06:30 +0200

    seDeplacer monstre

--- jeuZeldiablo/application/Monstre.java
+++ jeuZeldiablo/application/Monstre.java
@@ -1,5 +1,8 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Monstre extends Entite {
     private int x,y;
 
@@ -12,11 +15,26 @@ public class Monstre extends Entite {
         super(x,y);
     }
 
-    public int getX() {
-        return x;
-    }
+    /**
+     * permet le deplacement du monstre en se rapprochant du personnage
+     * @param libres cases libres autour du monstre
+     * @param p personnage que le monstre veut attaquer
+     */
+    public void seDeplacer(ArrayList<Case> libres, Personnage p) {
+        int prochainX = this.x;
+        int prochainY = this.y;
+        double meilleureDistance = 20;
 
-    public int getY() {
-        return y;
+        for (Case c : libres) {
+            double dist = Math.sqrt((p.getX()-this.x)*(p.getX()-this.x) + (p.getY()-this.y)*(p.getY()-this.y));
+            if (dist<meilleureDistance) {
+                prochainX = c.getX();
+                prochainY = c.getY();
+                meilleureDistance = dist;
+            }
+        }
+
+        this.x = prochainX;
+        this.y = prochainY;
     }
 }
