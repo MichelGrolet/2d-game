850c40abe7a193991af554f4b510efe43aa17f2c
Gabin Chassard <gabin.chassard2@etu.univ-lorraine.fr>
2021-06-08 16:54:35 +0200

    Classes CaseSpeciale et derivees

---
+++ jeuZeldiablo/application/CaseForce.java
@@ -0,0 +1,37 @@
+package application;
+
+/**
+* Classe abstraite representant une case speciale(piege,boost,etc...)
+*/
+public abstract class CaseForce extends CaseSpeciale{
+    private static final int BOOST=2;
+
+    /**
+    * Constructeur heritant de celui de CaseSpeciale
+    * @param x coordonnee x de la case
+    * @param y coordonnee y de la case
+    */
+    public CaseForce(int x,int y){
+        super(x,y);
+    }
+
+    /**
+    * Permet de declencher l'effet et passe la case en inactivable
+    * @param joueur le joueur qui subit l'effet
+    */   
+    public void declencherEffet(Personnage joueur){
+        //Si la case est activable, on active l'effet de la classe et on desactive la case
+        if(active){
+            effet(joueur);
+            this.active=true;
+        }
+    }
+
+    /**
+    * Reduit les points de vie du joueur s'il marche dessus
+    * @param joueur le joueur qui subit l'effet
+    */
+    public void effet(Personnage joueur){
+        joueur.setPuissance(joueur.getPuissance()+BOOST);
+    }
+}
