a4c02f686ea422c817a5fafb4594b879af9acb60
grolet4u <grolet4u@etu.univ-lorraine.fr>
2021-06-09 08:50:02 +0200

    fantome

--- jeuZeldiablo/application/Fantome.java
+++ jeuZeldiablo/application/Fantome.java
@@ -21,25 +21,27 @@ public class Fantome extends Monstre {
         this.pv = 5;
     }
 
-    public ArrayList<Case> DeplacementAutour(JeuPerso j) {
+    public ArrayList<Case> deplacementAutour(JeuPerso j) {
         // CasesAutour va contenir les 4 cases autour du fantome.
         ArrayList<Case> casesAutour = new ArrayList<Case>();
         ArrayList<Case> casesEntites = j.getCasesEntites();
         if (this.getX()==0) {
-            casesAutour.add(j.getLabyrinthe().getCase(this.getX()+1, this.getY()+1));
+            casesAutour.add(j.getLabyrinthe().getCase(this.getX()+1, this.getY()));
             if (this.getY()==0) {
-                casesAutour.add(j.getLabyrinthe().getCase(this.getX(), this.getY()));
-            } else if (this.getY()==Labyrinthe.TAILLE-1) casesAutour.add(j.getLabyrinthe().getCase(this.getX(), this.getY()-1));
-        } else if (this.getY()==Labyrinthe.TAILLE-1) casesAutour.add(j.getLabyrinthe().getCase(this.getX()-1, this.getY()));
-
-        // On enleve de casesAutour les cases contenant des entites
-        for (int i = 0; i< casesAutour.size();i++) {
-            for(int k = 0; i < casesEntites.size(); i++) {
-                if(casesAutour.get(i).getX() == casesEntites.get(k).getX() && casesAutour.get(i).getY()==casesEntites.get(k).getY()) {
-                    casesAutour.remove(i);
-                }
+                casesAutour.add(j.getLabyrinthe().getCase(this.getX(), this.getY()+1));
             }
+        } else if (this.getX()==Labyrinthe.TAILLE-1) {
+            casesAutour.add(j.getLabyrinthe().getCase(this.getX()-1, this.getY()));
+            if ( this.getY()==Labyrinthe.TAILLE-1) {
+                casesAutour.add(j.getLabyrinthe().getCase(this.getX(), this.getY()-1));
+            }
+        } else {
+            casesAutour.add(j.getLabyrinthe().getCase(this.getX()+1, this.getY()));
+            casesAutour.add(j.getLabyrinthe().getCase(this.getX(), this.getY()+1));
+            casesAutour.add(j.getLabyrinthe().getCase(this.getX(), this.getY()-1));
+            casesAutour.add(j.getLabyrinthe().getCase(this.getX()-1, this.getY()));
         }
+        System.out.println("dknnjnnnnnnnnnnnnnnnnnnnnn"+casesAutour.size());
         return casesAutour;
     }
 
